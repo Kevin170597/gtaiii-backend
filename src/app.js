6f1d609d33d9e95a11602f8916be34fe53f036c3
Kevin170597 <kevin170597@gmail.com>
2021-08-14 19:27:54 -0300

    hello world try

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.set("port", process.env.PORT || 3030)
+app.set(process.env.PORT || 3000)
 
 app.use(express.json());
 
 app.use(require("./routes/employees"));
 
-app.listen(3030, () => console.log("Server running at port 3030"));
+app.listen(3000, () => console.log("Server running at port 3000"));

b1e23648fd3d689de2668e0c0c9da623a5390988
Kevin170597 <kevin170597@gmail.com>
2021-10-06 00:39:17 -0300

    res.headers set

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ var PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(cors({
     origin: ["http://localhost:3000", "http://gtaiii.netlify.app"],
     methods: ["GET", "POST"],
--- src/database.js
+++ src/database.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: 'bc2jxk72l4oe9vqv4lue-mysql.services.clever-cloud.com',
     user: 'uzmedck4hiqbkjsf',
     password: 'NauZUeNS6DjaMawpYJqJ',
     database: 'bc2jxk72l4oe9vqv4lue'
 });
 
-mysqlConnection.connect(function (err) {
+/*mysqlConnection.connect(function (err) {
     if(err) {
         console.log(err);
         return;
     } else {
         console.log("DB is connected");
     }
-});
+});*/
 
 module.exports = mysqlConnection;

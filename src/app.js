861fc004561d91fc1a93b39d308a9ad090803b42
Kevin170597 <kevin170597@gmail.com>
2021-10-06 22:49:16 -0300

    cookies test on heroku

--- src/app.js
+++ src/app.js
@@ -37,9 +37,8 @@ app.use(session({
     secret: "gta3guide",
     resave: false,
     saveUninitialized: false,
-    proxy: false,
+    proxy: true,
     cookie: {
-        secure: true,
         expires: 365 * 24 * 60 * 60 * 1000
     },
     store: sessionStore,
